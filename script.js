3e98d17d094ab618877bc9259748e757fb27d336
rafameyun <raffinhah4@gmail.com>
2021-11-18 06:07:43 -0300

    Add event listener for submit

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 const apikey = "21d690cecb5c5cb0dfd36fe75846e1d5";
 
-const url = (location) => 
-`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-async function getWeatherByLocation(location) {
-    const resp = await fetch(url(location), {origin: "cors" });
+const url = (city) => 
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), {origin: "cors" });
     const respData = await resp.json();
 
+    console.log(respData);
+    
     addWeatherToPage(respData);
 }
 
 function addWeatherToPage(data) {
-    const temp = kToC(data.main.temp);
+    const temp = KtoC(data.main.temp);
 
-    const weather = document.createElement('div');
-    weather.classList.add('weather');
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
 
     weather.innerHTML = `
         <small> There are </small>
         <h2>${temp}°C</h2>
-        <p>in ${location}</p>
+        <p>in ${search.value}</p>
     `;
 
+    //Clean up 
+    main.innerHTML = "";
+
+    main.appendChild(weather);
+
 }
 
-function kToC(k) {
-    return (k - 273,15).toFixed(2);
+function KtoC(K) {
+    return Math.floor(K - 273,15).toFixed(1);
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+    
+    if(city) {
+        getWeatherByLocation(city);
+    }
+});
