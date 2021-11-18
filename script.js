1b717d0488c1781460db0357e2974a67187d5494
rafameyun <raffinhah4@gmail.com>
2021-11-18 05:03:12 -0300

    Add weather to page js file

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const apikey = "21d690cecb5c5cb0dfd36fe75846e1d5"
+const apikey = "21d690cecb5c5cb0dfd36fe75846e1d5";
 
 const url = (location) => 
 `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
 
 async function getWeatherByLocation(location) {
-    const resp = await fetch(url(location), {
-        origin: "cors" });
-        const respData = await resp.json();
+    const resp = await fetch(url(location), {origin: "cors" });
+    const respData = await resp.json();
 
-    console.log(respData);
+    addWeatherToPage(respData);
 }
 
-getWeatherByLocation('London');
+function addWeatherToPage(data) {
+    const temp = kToC(data.main.temp);
+
+    const weather = document.createElement('div');
+    weather.classList.add('weather');
+
+    weather.innerHTML = `
+        <small> There are </small>
+        <h2>${temp}°C</h2>
+        <p>in ${location}</p>
+    `;
+
+}
+
+function kToC(k) {
+    return (k - 273,15).toFixed(2);
+}
